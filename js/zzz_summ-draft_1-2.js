389d32f1beec46fa0dbd140122a13b61c426c2d2
YevhenRymarcuk <pkvoms@gmail.com>
2022-02-16 22:01:04 +0200

    Добавляет проработку конспекта по теме "Функции"

--- js/zzz_summ-draft_1-2.js
+++ js/zzz_summ-draft_1-2.js
@@ -293,3 +293,103 @@
 
 // console.log(slogan.strike());
 // console.log(optimisticSlogan);
+
+// ====================================================
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.slice(1, -1)); // ["Poly", "Kiwi"]
+
+// ====================================================
+
+// // Объявление параметров x, y, z
+// function multiply(x, y, z) {
+//   console.log(`Результат умножения равен ${x * y * z}`);
+// }
+
+// multiply(2, 2, 2, 2);
+
+// ====================================================
+
+// function multiply(x, y, z) {
+//   console.log("Код до return выполняется как обычно");
+
+//   console.log("Этот лог никогда не выполнится, он стоит после return");
+//   // Возвращаем результат выражения умножения
+// //   return x * y * z;
+
+// }
+
+// // Результат работы функции можно сохранить в переменную
+// let result = multiply(2, 3, 5);
+// console.log(result); // 30
+
+// result = multiply(4, 8, 12);
+// console.log(result); // 384
+
+// result = multiply(17, 6, 25);
+// console.log(result); // 2550
+
+// ====================================================
+
+// function count(countFrom = 0, countTo = 10, step = 1) {
+//   console.log(`countFrom = ${countFrom}, countTo = ${countTo}, step = ${step}`);
+
+//   for (let i = countFrom; i <= countTo; i += step) {
+//     console.log(i);
+//   }
+// }
+
+// count(1, 5); // countFrom = 1, countTo = 5, step = 1
+// count(2); // countFrom = 2, countTo = 10, step = 1
+// count(); // countFrom = 0, countTo = 10, step = 1
+
+// ====================================================
+
+// function multiply() {
+//   let total = 1;
+
+//   for (const argument of arguments) {
+//     total *= argument;
+//   }
+
+//   return total;
+// }
+
+// console.log(multiply(1, 5, 2)); //  6
+// console.log(multiply(1, 2, 3, 4)); //  24
+// console.log(multiply(1, 2, 3, 4, 5)); //  120
+
+// ====================================================
+
+// function fn(x, y, z) {
+//   // В переменной args будет полноценный массив
+//         const args = Array.from(arguments);
+        
+//         console.log(args);
+
+//         return x + y + y
+// }
+
+// let result = fn(2, 1, 2);
+// console.log(result);
+
+// ====================================================
+
+function withdraw(amount, balance) {
+  
+  if (amount === 0) {
+    console.log("Для проведения операции введите сумму больше нуля");
+    break;
+  }
+
+  if (amount > balance) {
+    console.log("Недостаточно средств на счету");
+    break;
+  }
+
+  console.log("Операция снятия средств проведена");
+}
+
+withdraw(0, 300); // "Для проведения операции введите сумму больше нуля"
+// withdraw(500, 300); // "Недостаточно средств на счету"
+// withdraw(100, 300); // "Операция снятия средств проведена"
