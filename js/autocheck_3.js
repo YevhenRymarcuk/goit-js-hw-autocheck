efcb1878ac818618b4351bfa7b6b2b332b599c17
YevhenRymarcuk <pkvoms@gmail.com>
2022-03-03 23:53:08 +0200

    Добавляет решение задач модуля 3 (с 10 по 25)

--- js/autocheck_3.js
+++ js/autocheck_3.js
@@ -355,79 +355,720 @@
 // console.log(values);
 
 /*=======================================================*/
-// Задача № 11: "------"
+// Задача № 11: "МЕТОД HASOWNPROPERTY()"
 
+/*
+* const keys = [];
+* const values = [];
+* const advert = {
+*   service: "apt",
+* };
+* const apartment = Object.create(advert);
+* apartment.descr = "Spacious apartment in the city center";
+* apartment.rating = 4;
+* apartment.price = 2153;
+* 
+* for (const key in apartment) {
+*   // Change code below this line
+* 
+*   keys.push(key);
+*   values.push(apartment[key]);
+* 
+*   // Change code above this line
+* }
+*/
 
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: "apt",
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = "Spacious apartment in the city center";
+// apartment.rating = 4;
+// apartment.price = 2153;
 
-/*=======================================================*/
-// Задача № 12: "------"
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//     // console.log(key);
+//   }
+//   // Change code above this line
+// }
 
-
-
-/*=======================================================*/
-// Задача № 13: "------"
-
-
-
-/*=======================================================*/
-// Задача № 14: "------"
-
-
-
-/*=======================================================*/
-// Задача № 15: "------"
-
-
-
-/*=======================================================*/
-// Задача № 16: "------"
-
-
-
-/*=======================================================*/
-// Задача № 17: "------"
-
-
-
-/*=======================================================*/
-// Задача № 18: "------"
-
-
-
-/*=======================================================*/
-// Задача № 19: "------"
-
-
-
-/*=======================================================*/
-// Задача № 20: "------"
-
-
+// console.log(apartment);
+// console.log(keys);
+// console.log(values);
 
 /*=======================================================*/
-// Задача № 21: "------"
+// Задача № 12: "ЗАДАЧА. ПОДСЧЁТ СВОЙСТВ"
 
+/*
+* function countProps(object) {
+*   let propCount = 0;
+*   // Change code below this line
+* 
+*   // Change code above this line
+*   return propCount;
+* }
+*/
 
+//// Вариант 1.
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (const key in object) {
+//     if (object.hasOwnProperty(key)) {
+//       propCount += 1;
+//     };
+//   };
+//   // Change code above this line
+//   return propCount;
+// };
+
+// /// Вариант 2.
+// function countProps(object) {
+// const ownKeys = Object.keys(object);
+// let propCount = 0;
+// // Change code below this line
+// for (const key of ownKeys) {
+//     propCount += 1;
+// };
+// // Change code above this line
+// return propCount;
+// };
+
+// console.log(countProps(apartment));
+// console.log(countProps(advert));
 
 /*=======================================================*/
-// Задача № 22: "------"
+// Задача № 13: "МЕТОД OBJECT.KEYS()"
 
+/*
+* const apartment = {
+*   descr: "Spacious apartment in the city center",
+*   rating: 4,
+*   price: 2153,
+* };
+* const values = [];
+* // Change code below this line
+* const keys = apartment;
+*/
 
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key]);
+// }
+
+// ///// Вариант с function
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const test = Object.create(apartment);
+
+// const ownProperty = function (object) {
+//     const values = [];
+//     const keys = Object.keys(object);
+
+//     for (const key of keys) {
+//         values.push(object[key]);
+//     };
+
+//     console.log(values);
+// }
+
+// ownProperty(apartment);
+// ownProperty(test);
 
 /*=======================================================*/
-// Задача № 23: "------"
+// Задача № 14: "ЗАДАЧА. ПОДСЧЁТ СВОЙСТВ 2.0"
 
+/*
+* function countProps(object) {
+*   // Change code below this line
+*   let propCount = 0;
+* 
+*   for (const key in object) {
+*     if (object.hasOwnProperty(key)) {
+*       propCount += 1;
+*     }
+*   }
+* 
+*   return propCount;
+*   // Change code above this line
+* }
+*/
 
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const test = Object.create(apartment);
+
+// ///// Вариант 1.
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+
+//   for (const key of keys) {
+//     propCount += 1;
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// };
+
+// ///// Вариант 2.
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+//   const keys = Object.keys(object);
+
+//     propCount = keys.length;
+
+//   return propCount;
+//   // Change code above this line
+// };
+
+// ///// Вариант 3.
+// function countProps(object) {
+//   // Change code below this line
+//     let propCount = 0;
+    
+//   propCount = Object.keys(object).length;
+
+//   return propCount;
+//   // Change code above this line
+// };
+
+// ///// Вариант 4.
+// function countProps(object) {
+//   // Change code below this line
+    
+//   return Object.keys(object).length;
+//   // Change code above this line
+// };
+
+// console.log(countProps(apartment));
+// console.log(countProps(test));
 
 /*=======================================================*/
-// Задача № 24: "------"
+// Задача № 15: "МЕТОД OBJECT.VALUES()"
 
+/*
+* const apartment = {
+*   descr: "Spacious apartment in the city center",
+*   rating: 4,
+*   price: 2153,
+* };
+* // Change code below this line
+* const keys = apartment;
+* const values = apartment;
+*/
 
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
+
+// console.log(keys);
+// console.log(values);
 
 /*=======================================================*/
-// Задача № 25: "------"
+// Задача № 16: "ЗАДАЧА. РАСХОДЫ НА ЗАРПЛАТУ"
+
+/*
+* function countTotalSalary(salaries) {
+*   let totalSalary = 0;
+*   // Change code below this line
+* 
+*   // Change code above this line
+*   return totalSalary;
+* };
+*/
+
+// const firstGroup = {
+//     Ваня: 100,
+//     Саша: 200,
+//     Валя: 300,
+// };
+
+// const secondGroup = {
+//     Коля: 200,
+//     Петя: 300,
+//     Вася: 500,
+// };
+
+// const totalGroup = { ...firstGroup, ...secondGroup };
+
+// const testGroup = Object.create(totalGroup);
+
+// //// Вариант 1.
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   for (const key in salaries) {
+//     if (salaries.hasOwnProperty(key)) {
+//       totalSalary += salaries[key];
+//     };
+//   };
+//   // Change code above this line
+//   return totalSalary;
+// };
+
+// //// Вариант 2.
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//   // Change code below this line
+//     const keys = Object.keys(salaries);
+
+//     for (const key of keys) {
+//         totalSalary += salaries[key];
+//     };
+//   // Change code above this line
+//     return totalSalary;
+// };
+
+// //// Вариант 3.
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//   // Change code below this line
+//     const keys = Object.values(salaries);
+
+//     for (const key of keys) {
+//         totalSalary += key;
+//     };
+//   // Change code above this line
+//     return totalSalary;
+// };
+
+// //// Вариант 4.
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//   // Change code below this line
+//     for (const value of Object.values(salaries)) {
+//         totalSalary += value;
+//     };
+//   // Change code above this line
+//     return totalSalary;
+// };
+
+// console.log(countTotalSalary(firstGroup));
+// console.log(countTotalSalary(secondGroup));
+// console.log(countTotalSalary(totalGroup));
+// console.log(countTotalSalary(testGroup));
+
+/*=======================================================*/
+// Задача № 17: "МАССИВ ОБЪЕКТОВ"
+
+/*
+* const colors = [
+*   { hex: "#f44336", rgb: "244,67,54" },
+*   { hex: "#2196f3", rgb: "33,150,243" },
+*   { hex: "#4caf50", rgb: "76,175,80" },
+*   { hex: "#ffeb3b", rgb: "255,235,59" },
+* ];
+* 
+* const hexColors = [];
+* const rgbColors = [];
+* // Change code below this line
+*/
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// };
+
+// console.table(hexColors);
+// console.table(rgbColors);
+
+/*=======================================================*/
+// Задача № 18: "ПОИСК ОБЪЕКТА ПО ЗНАЧЕНИЮ СВОЙСТВА"
+
+/*
+* const products = [
+*   { name: "Radar", price: 1300, quantity: 4 },
+*   { name: "Scanner", price: 2700, quantity: 3 },
+*   { name: "Droid", price: 400, quantity: 7 },
+*   { name: "Grip", price: 1200, quantity: 9 },
+* ];
+* 
+* function getProductPrice(productName) {
+*   // Change code below this line
+*   // Change code above this line
+* }
+*/
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) return product.price;
+//   }
+
+//   return null;
+//   // Change code above this line
+// }
+
+//// Вариант "Расширеный"))
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   for (const product of products) {
+//     if (product.name === productName) return `Цена товара с названием ${product.name} составляет ${product.price} кредитов.`;
+//   }
+
+//   return `Искомый товар не обнаружен: ${null}.`
+//   // Change code above this line
+// }
+
+// console.log(getProductPrice("Grap"));
+
+/*=======================================================*/
+// Задача № 19: "КОЛЛЕКЦИЯ ЗНАЧЕНИЙ СВОЙСТВА"
+
+/*
+* const products = [
+*   { name: "Radar", price: 1300, quantity: 4 },
+*   { name: "Scanner", price: 2700, quantity: 3 },
+*   { name: "Droid", price: 400, quantity: 7 },
+*   { name: "Grip", price: 1200, quantity: 9 },
+* ];
+* 
+* function getAllPropValues(propName) {
+*   // Change code below this line
+*   // Change code above this line
+* }
+*/
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
 
 
+// //// Вариант 1.
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let propValues = [];
+  
+//   for (const product of products) {
+//     if (Object.keys(product).includes(propName)) {
+//         propValues.push(product[propName]);
+//     };
+//   };
+
+//   return propValues;
+//   // Change code above this line
+// };
+
+// //// Вариант 2.
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   let propValues = [];
+  
+//     for (const product of products) {
+      
+//         for (const key of Object.keys(product)) {
+            
+//             if (key === propName) {
+//                 propValues.push(product[key]);
+//             };
+//         };
+//     };
+
+//   return propValues;
+//   // Change code above this line
+// };
+
+// console.log(getAllPropValues('name'));
+
+/*=======================================================*/
+// Задача № 20: "ОБЩАЯ СТОИМОСТЬ ТОВАРА"
+
+/*
+* const products = [
+*   { name: "Radar", price: 1300, quantity: 4 },
+*   { name: "Scanner", price: 2700, quantity: 3 },
+*   { name: "Droid", price: 400, quantity: 7 },
+*   { name: "Grip", price: 1200, quantity: 9 },
+* ];
+* 
+* function calculateTotalPrice(productName) {
+*   // Пиши код ниже этой строки
+*   // Пиши код выше этой строки
+* }
+*/
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   for (const product of products) {
+//     if (product.name === productName) return product.price * product.quantity;
+//   }
+
+//   return 0;
+//   // Пиши код выше этой строки
+// };
+
+// console.log(calculateTotalPrice('Grip'));
+
+/*=======================================================*/
+// Задача № 21: "ДЕСТРУКТУРИЗАЦИЯ ОБЪЕКТОВ"
+
+/*
+* const highTemperatures = {
+*   yesterday: 28,
+*   today: 26,
+*   tomorrow: 33,
+* };
+* // Change code below this line
+* 
+* const yesterday = highTemperatures.yesterday;
+* const today = highTemperatures.today;
+* const tomorrow = highTemperatures.tomorrow;
+* 
+* // Change code above this line
+* const meanTemperature = (yesterday + today + tomorrow) / 3;
+*/
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {yesterday, today, tomorrow} = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(`Средний показатель максимальных температур за 3 дня составляет ${meanTemperature} градусов.`);
+
+/*=======================================================*/
+// Задача № 22: "ЗНАЧЕНИЯ ПО УМОЛЧАНИЮ"
+
+/*
+* const highTemperatures = {
+*   yesterday: 28,
+*   today: 26,
+*   tomorrow: 33,
+* };
+* // Change code below this line
+* 
+* const yesterday = highTemperatures.yesterday;
+* const today = highTemperatures.today;
+* const tomorrow = highTemperatures.tomorrow;
+* const icon = highTemperatures.icon;
+* 
+* // Change code above this line
+* const meanTemperature = (yesterday + today + tomorrow) / 3;
+*/
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(`Средний показатель максимальных температур за 3 дня составляет ${meanTemperature} градусов.`);
+
+/*=======================================================*/
+// Задача № 23: "ИЗМЕНЕНИЕ ИМЕНИ ПЕРЕМЕННОЙ"
+
+/*
+* const highTemperatures = {
+*   yesterday: 28,
+*   today: 26,
+*   tomorrow: 33,
+* };
+* // Change code below this line
+* 
+* const highYesterday = highTemperatures.yesterday;
+* const highToday = highTemperatures.today;
+* const highTomorrow = highTemperatures.tomorrow;
+* const highIcon = highTemperatures.icon;
+* 
+* // Change code above this line
+* const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+*/
+
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+// (highYesterday + highToday + highTomorrow) % 3 !== 0
+//     ? console.log(`Средний показатель максимальных температур за 3 дня составляет ${meanTemperature.toFixed(1)} градусов.`)
+//     : console.log(`Средний показатель максимальных температур за 3 дня составляет ${meanTemperature} градусов.`);
+
+/*=======================================================*/
+// Задача № 24: "ДЕСТРУКТУРИЗАЦИЯ В ЦИКЛАХ"
+
+/*
+* const colors = [
+*   { hex: "#f44336", rgb: "244,67,54" },
+*   { hex: "#2196f3", rgb: "33,150,243" },
+*   { hex: "#4caf50", rgb: "76,175,80" },
+*   { hex: "#ffeb3b", rgb: "255,235,59" },
+* ];
+* 
+* const hexColors = [];
+* const rgbColors = [];
+* // Change code below this line
+* 
+* for (const color of colors) {
+*   hexColors.push(color.hex);
+*   rgbColors.push(color.rgb);
+* }
+*/
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// // //// Вариант 1.
+// // for (const color of colors) {
+// //   const {hex, rgb,} = color;
+// //   hexColors.push(hex);
+// //   rgbColors.push(rgb);
+// // };
+
+// //// Вариант 2.
+// for (const {hex, rgb,} of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// };
+
+// console.table(hexColors);
+// console.table(rgbColors);
+
+/*=======================================================*/
+// Задача № 25: "ГЛУБОКАЯ ДЕСТРУКТУРИЗАЦИЯ"
+
+/*
+* const forecast = {
+*   today: {
+*     low: 28,
+*     high: 32,
+*     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+*   },
+*   tomorrow: {
+*     low: 27,
+*     high: 31,
+*   },
+* };
+* // Change code below this line
+* 
+* const highToday = forecast.today.high;
+* const lowToday = forecast.today.low;
+* const todayIcon = forecast.today.icon;
+* 
+* const highTomorrow = forecast.tomorrow.high;
+* const lowTomorrow = forecast.tomorrow.low;
+* const tomorrowIcon = forecast.tomorrow.icon;
+*/
+
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
+
+// const {
+//   today: {
+//     high: highToday, 
+//     low: lowToday, 
+//     icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+//   tomorrow: {
+//     high: highTomorrow,
+//     low: lowTomorrow,
+//     icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+//   },
+// } = forecast;
+
+// console.log(highToday);
+// console.log(lowToday);
+// console.log(highTomorrow);
+// console.log(lowTomorrow);
 
 /*=======================================================*/
 // Задача № 26: "------"
